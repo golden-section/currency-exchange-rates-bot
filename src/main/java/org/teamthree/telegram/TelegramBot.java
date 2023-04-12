ffb4221ba037013cbd0ca41852087716c958ce69
Andrey Kolomoets <kolomoets02@gmail.com>
2023-04-12 21:34:00 +0300

    Added start menu + have bug with buttons

--- src/main/java/org/teamthree/telegram/TelegramBot.java
+++ src/main/java/org/teamthree/telegram/TelegramBot.java
@@ -2,8 +2,6 @@ package org.teamthree.telegram;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
